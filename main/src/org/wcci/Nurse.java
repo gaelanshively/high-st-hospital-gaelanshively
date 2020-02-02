d7c6ff5368ae24ac94f3850e06f5aa2731bb1088
Gaelan Shively <gaelanshively@gmail.com>
2020-02-01 23:39:38 -0500

    added nurse subclass

---
+++ main/src/org/wcci/Nurse.java
@@ -0,0 +1,8 @@
+package org.wcci;
+
+public class Nurse extends MedicalStaff {
+    public Nurse(String employeeName, double employeeSalary, int employeeIdentification, String employeePosition, int healingValue) {
+        super(employeeName, employeeSalary, employeeIdentification, employeePosition, healingValue);
+
+    }
+}
